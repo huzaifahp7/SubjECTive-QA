e6987ee70e808d132601b61b7025604e9fd3e95f
veer1104 <67676620+veer1104@users.noreply.github.com>
2023-10-13 21:36:07 -0400

    Update feature_generator.py
    Added the feature generating code for the csv files.

--- feature_generator.py
+++ feature_generator.py
@@ -39,18 +39,50 @@ def generate_features(question,answer):
     is an 'Aggressive answer'. In this case you would define an 'Aggression' variable with this answer and any more if needed.
     A few other examples could be: Clarity (vague vs clear) and Optimism (Optimistic vs Non-optimistic).
     Don't include any justification for the labels. Generate your answer in the following format:
-    'Here are 10 features based on the quality and tone of an answer:\n\n1. **Aggression:** 
-    The degree to which the answer is assertive or forceful.\n2. **Clarity:** The degree to which the answer is easy to understand.\n3. 
-    **Confidence:** The degree to which the answer is expressed with certainty.\n4. **Defensiveness:** The degree to which the answer is 
-    apologetic or evasive.\n5. **Optimism:** The degree to which the answer is hopeful or positive.\n6. **Passiveness:** The degree 
-    to which the answer is meek or submissive.\n7. **Politeness:** The degree to which the answer is respectful or 
-    considerate.\n8. **Relevance:** The degree to which the answer is related to the question.\n9. **Specificity:** The 
-    degree to which the answer is detailed and precise.\n10. **Tone:** The overall emotional quality of the answer.\n\nThese 
-    features can be used to assess the quality of an answer and to identify areas where the answer could be improved. For example, 
-    if an answer is unclear, the writer could be asked to provide more detail or to rephrase the answer in a clearer way. 
-    If an answer is defensive, the writer could be asked to be more assertive or to provide more evidence to support their claims.' '''
+    'Here are 10 features based on the quality and tone of an answer:\n\n1. **Aggression:**
+    The degree to which the answer is assertive or forceful.\n2. **Clarity:** The degree to which the answer is easy to understand.\n3.
+    **Confidence:** The degree to which the answer is expressed with certainty.\n4. **Defensiveness:** The degree to which the answer is
+    apologetic or evasive.\n5. **Optimism:** The degree to which the answer is hopeful or positive.\n6. **Passiveness:** The degree
+    to which the answer is meek or submissive.\n7. **Politeness:** The degree to which the answer is respectful or
+    considerate.\n8. **Relevance:** The degree to which the answer is related to the question.\n9. **Specificity:** The
+    degree to which the answer is detailed and precise.\n10. **Tone:** The overall emotional quality of the answer.\n\nThese
+    features can be used to assess the quality of an answer and to identify areas where the answer could be improved. For example,
+    if an answer is unclear, the writer could be asked to provide more detail or to rephrase the answer in a clearer way.
+    If an answer is defensive, the writer could be asked to be more assertive or to provide more evidence to support their claims.' Please note that the examples provided like Aggression, Clarity, Confidence, etc. are just examples and for you to understand how to produce the output.
+    You do not need to necessarily give those specific 10 words as an output. The 10 words can be any set of new words. '''
 
     res = palm.chat(messages = prompt)
     output_list.append(res.last)
     output_list = cleaner(output_list[0])
     return output_list
+
+
+directory_path = "/content/data"
+
+# Initialize an empty list to store the results
+results = []
+
+# Loop through all CSV files in the directory
+for filename in os.listdir(directory_path):
+    if filename.endswith(".csv"):
+        file_path = os.path.join(directory_path, filename)
+
+        # Read the CSV file into a DataFrame
+        df = pd.read_csv(file_path)
+
+        # Iterate through each row and apply the generate_features function
+        for index, row in df.iterrows():
+            question = row["Question"]
+            answer = row["Answer"]
+
+            # Call the generate_features function
+            output_list = generate_features(question, answer)
+
+            # Append the result to the list
+            results.append(output_list)
+
+# Create a new DataFrame from the results list
+output_df = pd.DataFrame(results, columns=["Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7", "Feature 8", "Feature 9", "Feature 10"])
+
+# Save the resulting DataFrame to a CSV file if needed
+output_df.to_csv("/content/data/output_results.csv", index=False)
