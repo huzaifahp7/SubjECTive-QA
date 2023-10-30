b671ea926d719f40a55dfc4d586a09afabd6f6ac
Huzaifa Pardawala <hp@lawn-128-61-75-106.lawn.gatech.edu>
2023-10-30 14:48:06 -0400

    edit feature adder

--- feature_adder.py
+++ feature_adder.py
@@ -4,11 +4,12 @@ import pandas as pd
 # List of features
 features = ["Clear", "Assertive", "Cautious", "Optimistic", "Specific", "Relevant"]
 
-# Path to the 'cleaned_transcripts' directory
-directory_path = "cleaned_transcripts"
+dir_path1 = "datasets"
+
+directory_path = "manual_annotations"
 
 # Iterate through transcript files and add empty columns
-for filename in os.listdir(directory_path):
+for filename in os.listdir(dir_path1):
     if filename.endswith(".csv"):  # Assuming the transcripts are in CSV format
         file_path = os.path.join(directory_path, filename)
         df = pd.read_csv(file_path)
@@ -19,3 +20,15 @@ for filename in os.listdir(directory_path):
 
         # Save the modified DataFrame back to the file
         df.to_csv(file_path, index=False)
+        
+    if filename.endswith(".xlsx"):
+        file_path = os.path.join(directory_path, filename)
+        df = pd.read_excel(file_path)
+
+        # Add empty columns for each feature
+        for feature in features:
+            df[feature] = None
+
+        # Save the modified DataFrame back to the file
+        df.to_excel(file_path, index=False)
+        
