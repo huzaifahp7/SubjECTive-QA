f7c85202582a850ee65f5df073bf33e0a056a37d
Huzaifa Pardawala <hp@lawn-128-61-75-106.lawn.gatech.edu>
2023-10-30 15:49:11 -0400

    edit counter for general use

--- counter.py
+++ counter.py
@@ -1,4 +1,14 @@
+import os
 count1 = 0
 count2 = 0
 dir1 = "datasets"
-dir2 = = "manual_annotations"
+dir2 = "manual_annotations"
+
+for file in os.listdir(dir1):
+    count1 += 1
+for file in os.listdir(dir2):
+    count2 +=1
+    
+    
+print(count1)
+print(count2)
