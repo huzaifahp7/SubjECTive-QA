5aa6d8b6de3f89ace9e860b77d44ed0c0b6eff7e
Huzaifa Pardawala <hp@ipsec-10-2-129-5.vpn.gatech.edu>
2023-12-03 03:31:27 -0500

    abhi

--- pages/Page 2.py
+++ pages/Page 2.py
@@ -19,17 +19,36 @@ tonality_df = result_df[tonality_features]
 # Calculating the correlation matrix
 correlation_matrix = tonality_df.corr()
 
+# Create annotations with correlation values
+annotations = [
+    dict(
+        x=i,
+        y=j,
+        text=str(round(correlation_matrix.iloc[i, j], 2)),
+        showarrow=False,
+        font=dict(color='black'),
+    )
+    for i in range(len(tonality_features))
+    for j in range(len(tonality_features))
+]
+
 # Streamlit app
 st.title("Interactive Correlation Matrix")
 
 # Create an interactive heatmap using Plotly
-fig = px.imshow(correlation_matrix, labels=dict(color="Correlation"), x=tonality_features, y=tonality_features,
-                color_continuous_scale='Viridis', zmin=-1, zmax=1)
+fig = px.imshow(correlation_matrix,
+                labels=dict(color="Correlation"), 
+                x=tonality_features, 
+                y=tonality_features,
+                color_continuous_scale='RdBu', 
+                zmin=-1, 
+                zmax=1)
 
 # Set title
 fig.update_layout(title_text='Correlation Matrix of Tonality Features')
 
+# Add correlation values as annotations
+fig.update_layout(annotations=annotations)
+
 # Display the plot using Streamlit
 st.plotly_chart(fig)
-
-
